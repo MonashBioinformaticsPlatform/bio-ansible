ad03697dace1fd07f9abaf711fb9c127ae2ff5bf
serine <tskiril@gmail.com>
2017-01-25 14:55:01 +1100

    worked on make_users.bash script

--- scripts/BASH/make_users.bash
+++ scripts/BASH/make_users.bash
@@ -2,22 +2,51 @@
 
 make_user() {
 
-  made_pass=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c7 | mkpasswd --stdin`
-  #user_group="unknown"
-  #useradd --password $made_pass \
-  #        --create-home \
-  #        --shell /bin/bash \
-  #        "$new_guy"
+  new_guy=$1
+
+  if ! grep -q $new_guy /etc/passwd
+  then
+    made_rand=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c7`
+    made_pass=`mkpasswd --method=sha-256 $made_rand`
+    #user_group="unknown"
+    #useradd --password $made_pass \
+    #        --create-home \
+    #        --shell /bin/bash \
+    #        "$new_guy"
+    echo "$new_guy,$made_rand,new"
+  else
+    >&2 echo "MESSAGE: Bad username $new_guy"
+    echo "$new_guy,-,current"
+  fi
 }
 
-new_guy=""
+run_through() {
 
-echo "This is input $1 file"
+  input_file=$1
 
-while read line
-do
-  make_user
-  echo "$line,$made_pass" >> users_list.txt
-done < $1
+  while read dude pass_word user_status
+  do
+    if [[ -z $user_status ]]
+    then
+        make_user $dude
+    elif [[ $user_status == "current" ]]
+    then
+        make_user $dude
+    elif [[ $user_status == "new" ]]
+    then
+      echo "$dude,$pass_word,$user_status"
+    else
+      >2& echo "ERROR: This shouldn't have happend"
+    fi
 
-unset made_pass
+  done < $input_file
+  
+  unset made_rand
+  unset made_pass
+  unset new_guy
+}
+
+old_ifs=$IFS
+IFS=,
+
+run_through $1
