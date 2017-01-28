4df973a4234014490f3bde7dc0dfcd1100f8a76e
Create launching guy <ubuntu@ansible-launcher.novalocal>
2017-01-28 11:41:43 +0000

    worked on mkuser script

--- scripts/BASH/mkuser.bash
+++ scripts/BASH/mkuser.bash
@@ -18,7 +18,7 @@ check_user() {
 }
 
 send_out() {
-    echo "This is your username $new_guy and randomly generate password $made_rand. You are strongly encouraged to change it. You can do so by running passwd command and following the prompts \\n \\n Not a lab guy" | mail -s "Your login details for upcoming course by Monash Bioinforamtics Platform" $user_mail
+    printf "This is your username $new_guy and randomly generate password $made_rand. \n You are strongly encouraged to change it. You can do so by running \`passwd\` command and following the prompts \n \n Not a lab guy" | mail -s "Your login details for upcoming course by Monash Bioinforamtics Platform" $user_mail
 }
 
 make_user() {
@@ -28,7 +28,7 @@ make_user() {
 
   if [[ -n $new_guy ]]
   then
-    if [[ $EUID == 0]]
+    if [[ $EUID == 0 ]]
     then
       made_rand=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c7`
       made_pass=`mkpasswd --method=sha-256 $made_rand`
@@ -40,7 +40,7 @@ make_user() {
       echo "$new_guy,$user_mail,new,$made_rand"
       send_out
     else
-      >2& echo "ERROR: Are you running this script as root? You sould be !"
+      >&2 echo "ERROR: Are you running this script as root? You sould be !"
     fi
   else
     #>&2 echo "MESSAGE: Bad username $new_guy"
@@ -55,6 +55,21 @@ make_user() {
   unset made_pass
 }
 
+del_user() {
+  old_guy=$1
+
+  if [[ -n $old_guy ]]
+  then
+    if [[ $EUID == 0 ]]
+    then
+      userdel --remove $old_guy
+      >&2 echo "MESSAGE: Username $old_guy had been removed from the system, including howe directory"
+    else
+      >&2 echo "ERROR: Are you running this script as root? You sould be !"
+    fi
+  fi
+}
+
 make_users() {
 
   input_file=$1
@@ -66,64 +81,62 @@ make_users() {
         make_user $user_name $user_mail
     elif [[ $user_status == "current" ]]
     then
-        make_user $user_name $user_mail
+      echo "$user_name,$user_mail,$user_status,-"
     elif [[ $user_status == "new" ]]
     then
+      make_user $user_name $user_mail
       echo "$user_name,$user_mail,$user_status,$pass_word"
+    elif [[ $user_status == "old" ]]
+    then
+      del_user $user_name
+      echo "$user_name,$user_mail,old,-"
     else
-      >2& echo "ERROR: This shouldn't have happend"
+      >&2 echo "ERROR: This shouldn't have happend"
     fi
 
   done < $input_file
   
 }
 
-main() {
+#main() {
 
-  while [[ $# -gt 0 ]]
-  do
-    key="$1"
+while [[ $# -gt 0 ]]
+do
+  key="$1"
 
-    case $key in
-      (-h|--help)
-        echo ""
-        echo "  Version: 0.1.2"
-        echo "  Usage: mkuser [OPTIONS] < --filename | --username >"
-        echo ""
-        echo "  Options: "
-        echo ""
-        echo "           -f (--filename) <FILE> - provide file with user infomation"
-        #echo "           -s (--show) - show user(s) status on the machine"
-        #echo "           -n (--newpass) - generate new random password for user(s)"
-        echo "           -d (--delete) - remove user(s) from the system" 
-        echo "           --ifs [,] - set IFS (internal field separator)"
-        echo ""
-        ;;
-      (-f|--filename)
-        if [[ -f $2 && -s $2 ]]
-        then
-          mkusers
-        else
-          >2& echo "ERROR: Check your input file --filename $2"
-        fi
-        shift
-        ;;
-       (-d|--delete)
-          #del_users
-          echo "Need to implement delete users "
-          shift
-          ;;
-        (*)
-          >2& echo "ERROR: please check your input"
-          main --help
-          ;;
-        (--ifs)
-          IFS=$3
-          shift
-          ;;
-      esac
-  done
-}
+  case $key in
+    (-h|--help)
+      echo ""
+      echo "  Version: 0.1.2"
+      echo "  Usage: mkuser [OPTIONS] < --filename FILE > "
+      echo ""
+      echo "  Options: "
+      echo ""
+      echo "           -f (--filename) <FILE> - provide file with user infomation"
+      #echo "           -s (--show) - show user(s) status on the machine"
+      #echo "           -n (--newpass) - generate new random password for user(s)"
+      #echo "           -d (--delete) - remove user(s) from the system" 
+      echo "           --ifs [,] - set IFS (internal field separator)"
+      echo ""
+      shift
+      ;;
+    (-f|--filename)
+      if [[ -f $2 && -s $2 ]]
+      then
+        make_users $2
+      else
+        >&2 echo "ERROR: Check your input file --filename $2"
+      fi
+      shift
+      ;;
+    (--ifs)
+      IFS=$3
+      shift
+      ;;
+  esac
+  shift
+done
+#}
 
 IFS=$old_ifs
 
@@ -132,6 +145,5 @@ unset check_user
 unset send_out
 unset make_user
 unset make_users
-unset main
+#unset main
 
-main 
