766fd800e9d858eab9f1aa64a6d6154e280889d9
serine <tskiril@gmail.com>
2017-02-07 21:56:47 +1100

    updated lmod StdEenv template

--- roles/lmod/templates/z00_StdEnv.sh
+++ roles/lmod/templates/z00_StdEnv.sh
@@ -1,4 +1,9 @@
-if [ -z "$__Init_Default_Modules" -o -z "$LD_LIBRARY_PATH" ]; then
-  __Init_Default_Modules=1; export __Init_Default_Modules;
-  module getdefault default || module load StdEnv
+if [ -z "$__Init_Default_Modules" ]; then
+  export __Init_Default_Modules=1;
+  ## ability to predefine elsewhere the default list
+  LMOD_SYSTEM_DEFAULT_MODULES=${LMOD_SYSTEM_DEFAULT_MODULES:-"StdEnv"}
+  export LMOD_SYSTEM_DEFAULT_MODULES
+  module --initial_load restore
+else
+  module refresh
 fi
