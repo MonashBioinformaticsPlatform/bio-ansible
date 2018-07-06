222903b6a7a32248fc75036588f4d491477b6e28
Andrew Perry <ajperry@pansapiens.com>
2018-07-06 21:56:33 +1000

    More typos. Nuf said.

--- docker/generate_travis_biotool_docker_lines.sh
+++ docker/generate_travis_biotool_docker_lines.sh
@@ -9,5 +9,5 @@ ansible-playbook --list-tags bio.yml | \
   grep "TASK TAGS:" | \
   sed "s/TASK TAGS: \[//" | \
   python3 -c "import sys; tags=sys.stdin.read().split(','); \
-              [print('  - travis_wait 300 docker build --build-arg TASK_TAGS={tag} -t bioansible:{tag} -f docker/Dockerfile-bio-tags-travis .'\
+              [print('  - travis_wait 300 docker build --pull=false --build-arg TASK_TAGS={tag} -t bioansible:{tag} -f docker/Dockerfile-bio-tags-travis .'\
               .format(tag=t.strip())) for t in tags]"
