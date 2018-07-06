9797352e64a4a5e8612beba9b9adf44eaad53520
Andrew Perry <ajperry@pansapiens.com>
2018-07-06 15:38:28 +1000

    Let's try testing one tool per container builds on Travis.

---
+++ scripts/other/generate_travis_biotool_docker_lines.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Takes every tag defined in the bio.yml role and outputs a Docker build line
+# to test it in isolation on Travis CI.
+# Add the output to the scripts section of .travis.yml
+#
+
+ansible-playbook --list-tags bio.yml | \
+  grep "TASK TAGS:" | \
+  sed "s/TASK TAGS: \[//" | \
+  python3 -c "import sys; tags=sys.stdin.read().split(','); \
+              [print('  - travis_wait 300 docker build --build-arg TASK_TAGS={tag} -t bioansible:{tag} -f docker/Dockerfile-bio-tags .'\
+              .format(tag=t.strip())) for t in tags]"
