bef77a3aa017c50e325a82a185ee983dfb549e0d
Andrew Perry <ajperry@pansapiens.com>
2019-04-30 14:05:57 +1000

    Moved shared lmod templates to top level of playbook.

--- roles/lmod_etc/templates/z00_StdEnv.sh
+++
@@ -1,9 +0,0 @@
-if [ -z "$__Init_Default_Modules" ]; then
-  export __Init_Default_Modules=1;
-  ## ability to predefine elsewhere the default list
-  LMOD_SYSTEM_DEFAULT_MODULES=${LMOD_SYSTEM_DEFAULT_MODULES:-"StdEnv"}
-  export LMOD_SYSTEM_DEFAULT_MODULES
-  module --initial_load restore
-else
-  module refresh
-fi
