a7849460f93874ceaf381b33f7334a3e87bfaadc
Andrew Perry <ajperry@pansapiens.com>
2020-07-16 15:01:59 +1000

    Disable RStudio Pro installation by default. Add some apt packages. Add robots.txt to nginx config. Fix Bioconductor installation. Some TODOs.

--- scripts/R/bioconductor.R
+++ scripts/R/bioconductor.R
@@ -1,26 +1,35 @@
 ## try http:// if https:// URLs are not supported
-source("https://bioconductor.org/biocLite.R")
-# Install base BioConductor
-biocLite(ask=FALSE)
-#upgrade bioconductor installer
-#biocLite("BiocUpgrade")
-# Install the BioConductor packages we want
-biocLite(pkgs = c("Biostrings",
-                  "GenomicRanges",
-                  "GenomicFeatures",
-                  "BSgenome",
-                  "rtracklayer",
-                  "plyranges",
-                  "motifRG",
-                  "Rsubread",
-                  "edgeR",
-                  "limma",
-                  "biomaRt",
-                  "org.Hs.eg.db",
-                  "scater",
-                  "getopt",
-                  "MASS",
-                  "GOstats",
-                  "DESeq2",
-                  "metagenomeSeq")
-         )
+r = getOption("repos")
+r["CRAN"] = "https://mirror.aarnet.edu.au/pub/CRAN/"
+options(repos = r)
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install()
+
+
+# source("https://bioconductor.org/biocLite.R")
+# # Install base BioConductor
+# biocLite(ask=FALSE)
+# #upgrade bioconductor installer
+# #biocLite("BiocUpgrade")
+# # Install the BioConductor packages we want
+# biocLite(pkgs = c("Biostrings",
+#                   "GenomicRanges",
+#                   "GenomicFeatures",
+#                   "BSgenome",
+#                   "rtracklayer",
+#                   "plyranges",
+#                   "motifRG",
+#                   "Rsubread",
+#                   "edgeR",
+#                   "limma",
+#                   "biomaRt",
+#                   "org.Hs.eg.db",
+#                   "scater",
+#                   "getopt",
+#                   "MASS",
+#                   "GOstats",
+#                   "DESeq2",
+#                   "metagenomeSeq")
+#          )
